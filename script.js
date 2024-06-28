8f7d2322ca5f53fe7a8be44f413393dba8e20b9a
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2024-06-28 13:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ class Person {
 }
 
 class Student extends Person {
-	super(name,age);
+	// super(name,age);
 	study(){
 		console.log(`${this.name} is studying`)
 	}
 }
 
 class Teacher extends Person {
-	super(name,age);
+	// super(name,age);
 	teach(){
 		console.log(`${this.name} is teaching`)
 	}
